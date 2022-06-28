61cc6d7916dada822d6661144468dfb5dfa038d3
Yuriy Belekanych <yuriy.belekanych@gmail.com>
2022-06-28 23:50:38 +0300

    Refactored hard bot.

--- server/src/session/bots/hard.bot.ts
+++ server/src/session/bots/hard.bot.ts
@@ -16,59 +16,75 @@ export class Hard extends Base implements BotInterface {
   protected destroyed = 0
 
   protected getSpecificMove(field: Field): Move {
-    this.detectDeleted(field)
+    return this.findMax(
+      this.calculateProbability(
+        field,
+        this.findShip(field)
+      )
+    )
+  }
 
-    const ship = Math.max(...this.ships)
+  protected findShip(field): number {
+    this.detectDestroyed(field)
 
-    // Calculate probability
+    return Math.max(...this.ships)
+  }
+
+  protected detectDestroyed(field: Field): void {
+    const destroyed = field.getAmount(cell => cell === Cell.DESTROYED)
+
+    const destroyedShip =  destroyed - this.destroyed
+    const destroyedShipIndex = this.ships.findIndex(ship => ship === destroyedShip)
+
+    if (destroyedShipIndex !== -1) {
+      this.ships.splice(destroyedShipIndex, 1)
+    }
+
+    this.destroyed = destroyed
+  }
+
+  protected calculateProbability(field: Field, ship: number): Field {
     const probability = new Field(field.size())
     
-    for (let rowIndex = 0; rowIndex < field.size(); rowIndex++) {
-      for (let colIndex = 0; colIndex < field.size() - ship + 1; colIndex++) {
-        let hasConflict = false
-
-        for (let offset = 0; offset < ship; offset++) {
-          if (field.get(rowIndex, colIndex + offset) !== Cell.EMPTY) {
-            hasConflict = true
-            break
-          }
-        }
-
-        if (!hasConflict) {
+    for (let isHorizontal = 0; isHorizontal <= 1; isHorizontal++) {
+      for (let i = 0; i < field.size(); i++) {
+        for (let j = 0; j < field.size() - ship + 1; j++) {
+          let hasConflict = false
+  
           for (let offset = 0; offset < ship; offset++) {
-            probability.rows[rowIndex][colIndex + offset]++
-          }
-        }
-      }
-    }
-        
-    for (let colIndex = 0; colIndex < field.size(); colIndex++) {
-      for (let rowIndex = 0; rowIndex < field.size() - ship + 1; rowIndex++) {
-        let hasConflict = false
+            const rowIndex = isHorizontal ? i : j + offset
+            const colIndex = isHorizontal ? j + offset : i
 
-        for (let offset = 0; offset < ship; offset++) {
-          if (field.get(rowIndex + offset, colIndex) !== Cell.EMPTY) {
-            hasConflict = true
-            break
+            if (field.get(rowIndex, colIndex) !== Cell.EMPTY) {
+              hasConflict = true
+              break
+            }
           }
-        }
+  
+          if (!hasConflict) {
+            for (let offset = 0; offset < ship; offset++) {
+              const rowIndex = isHorizontal ? i : j + offset
+              const colIndex = isHorizontal ? j + offset : i
 
-        if (!hasConflict) {
-          for (let offset = 0; offset < ship; offset++) {
-            probability.rows[rowIndex + offset][colIndex]++
+              probability.rows[rowIndex][colIndex + offset]++
+            }
           }
         }
       }
     }
 
+    return probability
+  }
+
+  protected findMax(probability: Field): Move {
     let max = {
-      value: 0,
+      value: -1,
       row: null,
       col: null,
     }
 
     probability.forEach((value, row, col) => {
-      if (value >= max.value) {
+      if (value > max.value) {
         max = {
           value,
           row,
@@ -81,18 +97,5 @@ export class Hard extends Base implements BotInterface {
       row: max.row,
       col: max.col,
     }
-  }
-
-  protected detectDeleted(field: Field): void {
-    const destroyed = field.getAmount(cell => cell === Cell.DESTROYED)
-
-    const destroyedShip =  destroyed - this.destroyed
-    const destroyedShipIndex = this.ships.findIndex(ship => ship === destroyedShip)
-
-    if (destroyedShipIndex !== -1) {
-      this.ships.splice(destroyedShipIndex, 1)
-    }
-
-    this.destroyed = destroyed
   }
 }
