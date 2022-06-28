698c3bb3ca2791d386d0eed24a61ce4d505e521f
Yuriy Belekanych <yuriy.belekanych@gmail.com>
2022-06-28 22:03:23 +0300

    Added hard bot.

--- server/src/session/bots/base.bot.ts
+++ server/src/session/bots/base.bot.ts
@@ -1,11 +1,143 @@
-import { BotInterface } from './bot.interface'
-import { Player } from '../entities/player.entity'
 import Cell from '../enums/cell.enum'
-import { Field } from '../entities/field.entity'
+import Direction from '../enums/direction.enum';
 import Move from '../types/move.type'
+import { BotInterface } from './bot.interface'
+import { Field } from '../entities/field.entity'
+import { Player } from '../entities/player.entity'
+
+const DIRECTIONS: Direction[] = [
+  Direction.TOP,
+  Direction.RIGHT,
+  Direction.BOTTOM,
+  Direction.LEFT,
+]
+
+const DEFAULT_DIRECTION = 0
+const DEFAULT_OFFSET = 1
 
 export abstract class Base extends Player implements BotInterface {
   protected previousMove: Move|null
+  protected currentDirection: number = DEFAULT_DIRECTION
+  protected target: Move|null
+
+  public getNextMove(field: Field): Move {
+    if (!this.target) {
+      if (this.wasHit(field)) {
+        this.target = this.previousMove
+      }
+    } else {
+      if (this.wasDestroyed(field)) {
+        this.target = this.findNewTarget(field)
+        this.currentDirection = DEFAULT_DIRECTION
+      }
+    }
+
+    if (!this.target) {
+      return this.saveMove(this.getSpecificMove(field))
+    }
+
+    return this.saveMove(this.findNextHit(field))
+  }
+
+  protected getSpecificMove(field: Field): Move {
+    return this.getRandomMove(field)
+  }
+
+  protected wasHit(field: Field): boolean {
+    if (!this.previousMove) {
+      return false
+    }
+
+    return field.get(this.previousMove.row, this.previousMove.col) === Cell.HIT
+  }
+
+  protected wasDestroyed(field: Field): boolean {
+    return field.rows[this.previousMove.row][this.previousMove.col] === Cell.DESTROYED
+  }
+
+  protected findNewTarget(field: Field): Move|null {
+    for (let row = 0; row < field.rows.length; row++) {
+      for (let col = 0; col < field.rows[row].length; col++) {
+        if (field.rows[row][col] === Cell.HIT) {
+          return {
+            row,
+            col,
+          }
+        }
+      }
+    }
+
+    // Target not found
+    return null
+  }
+  
+  protected findNextHit(field: Field): Move {
+    let direction: Direction
+    let offset = DEFAULT_OFFSET
+    let move: Move
+
+    do {
+      direction = DIRECTIONS[this.currentDirection]
+      const possibleMove = this.getByDirection(direction, offset)
+
+      if (this.canHit(field, possibleMove)) {
+        if (this.isHit(field, possibleMove)) {
+          offset++
+        } else {
+          move = possibleMove
+        }
+      } else {
+        offset = DEFAULT_OFFSET
+        this.currentDirection++
+      }
+    } while (!move)
+
+    return move
+  }
+
+  protected getByDirection(direction: Direction, offset: number): Move {
+    let { row, col } = this.target
+
+    switch (direction) {
+      case Direction.TOP:
+        row -= offset
+        break
+      case Direction.RIGHT:
+        col += offset
+        break
+      case Direction.BOTTOM:
+        row += offset
+        break
+      case Direction.LEFT:
+        col -= offset
+        break
+    }
+
+    return {
+      row,
+      col,
+    }
+  }
+
+  protected canHit(field: Field, move: Move): boolean {
+    if (move.row < 0 || move.col < 0 || move.row >= field.size() || move.col >= field.size()) {
+      return false
+    }
+
+    if (field.get(move.row, move.col) === Cell.MISS) {
+      return false
+    }
+
+    if (field.get(move.row, move.col) === Cell.DESTROYED) {
+      return false
+    }
+
+    return true
+  }
+
+  protected isHit(field: Field, move: Move): boolean {
+    return field.get(move.row, move.col) === Cell.HIT
+  }
 
   protected getRandomMove(field: Field): Move {
     let row = 0;
--- server/src/session/bots/hard.bot.ts
+++ server/src/session/bots/hard.bot.ts
@@ -1,6 +1,98 @@
+import Move from '../types/move.type'
 import { Base } from './base.bot'
 import { BotInterface } from './bot.interface'
+import { Field } from '../entities/field.entity'
+import Cell from '../enums/cell.enum'
 
 export class Hard extends Base implements BotInterface {
-  //
+  protected ships = [
+    5,
+    4,
+    3,
+    3,
+    2,
+  ]
+
+  protected destroyed = 0
+
+  protected getSpecificMove(field: Field): Move {
+    this.detectDeleted(field)
+
+    const ship = Math.max(...this.ships)
+
+    // Calculate probability
+    const probability = new Field(field.size())
+    
+    for (let rowIndex = 0; rowIndex < field.size(); rowIndex++) {
+      for (let colIndex = 0; colIndex < field.size() - ship + 1; colIndex++) {
+        let hasConflict = false
+
+        for (let offset = 0; offset < ship; offset++) {
+          if (field.get(rowIndex, colIndex + offset) !== Cell.EMPTY) {
+            hasConflict = true
+            break
+          }
+        }
+
+        if (!hasConflict) {
+          for (let offset = 0; offset < ship; offset++) {
+            probability.rows[rowIndex][colIndex + offset]++
+          }
+        }
+      }
+    }
+        
+    for (let colIndex = 0; colIndex < field.size(); colIndex++) {
+      for (let rowIndex = 0; rowIndex < field.size() - ship + 1; rowIndex++) {
+        let hasConflict = false
+
+        for (let offset = 0; offset < ship; offset++) {
+          if (field.get(rowIndex + offset, colIndex) !== Cell.EMPTY) {
+            hasConflict = true
+            break
+          }
+        }
+
+        if (!hasConflict) {
+          for (let offset = 0; offset < ship; offset++) {
+            probability.rows[rowIndex + offset][colIndex]++
+          }
+        }
+      }
+    }
+
+    let max = {
+      value: 0,
+      row: null,
+      col: null,
+    }
+
+    probability.forEach((value, row, col) => {
+      if (value >= max.value) {
+        max = {
+          value,
+          row,
+          col,
+        }
+      }
+    })
+
+    return {
+      row: max.row,
+      col: max.col,
+    }
+  }
+
+  protected detectDeleted(field: Field): void {
+    const destroyed = field.getAmount(cell => cell === Cell.DESTROYED)
+
+    const destroyedShip =  destroyed - this.destroyed
+    const destroyedShipIndex = this.ships.findIndex(ship => ship === destroyedShip)
+
+    if (destroyedShipIndex !== -1) {
+      this.ships.splice(destroyedShipIndex, 1)
+    }
+
+    this.destroyed = destroyed
+  }
 }
